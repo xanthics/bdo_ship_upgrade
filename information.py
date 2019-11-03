59bc34a4cef4b55112938bfa789cbf78a5159e8e
xanthics <3137623+xanthics@users.noreply.github.com>
2019-11-02 19:31:25 -0700

    Added 100% durability requirement note.  Switched to P instead of BR tags

--- information.py
+++ information.py
@@ -135,8 +135,9 @@ def gen_info():
 		["Epheria Caravel", "Epheria Galleass"],
 		["Carrack (Advance)", "Carrack (Balance)", "Carrack (Volante)", "Carrack (Valor)"]
 	]
-	ret = "More information can be found " + A("at this spreadsheet", href="https://docs.google.com/document/d/1basknMfrfcH6AzJD9PkzeUunqrIGTuS6SfXPf3a7pso/preview", target="_blank") +" or " + A("these patch notes", href="https://www.blackdesertonline.com/news/view/3216", target="_blank") + BR() + BR()
-	ret += "Barter items that you can trade for ship parts unlock as you finish more trades.  You can always trade t1 barter items for verdant stone coupon though." + BR()
+	ret = P("More information can be found " + A("at this spreadsheet", href="https://docs.google.com/document/d/1basknMfrfcH6AzJD9PkzeUunqrIGTuS6SfXPf3a7pso/preview", target="_blank") +" or " + A("these patch notes", href="https://www.blackdesertonline.com/news/view/3216", target="_blank"))
+	ret += P("Barter items that you can trade for ship parts unlock as you finish more trades.  You can always trade t1 barter items for verdant stone coupon though.")
+	ret += P("Ship parts used for upgrade need to be full durability.")
 	ret += H2("Upgrade Paths") + BR() + CANVAS(id='shipchart', width=820, height=400) + BR()
 	ret += H2("Base Ship Stats")
 	for table in order:
