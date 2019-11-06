e53cdd0a32b6261c16ee79fa0d54f75dbbc56b1d
xanthics <3137623+xanthics@users.noreply.github.com>
2019-11-05 21:33:48 -0800

    small note about monster drops

--- information.py
+++ information.py
@@ -138,6 +138,7 @@ def gen_info():
 	ret = P("More information can be found " + A("at this spreadsheet", href="https://docs.google.com/document/d/1basknMfrfcH6AzJD9PkzeUunqrIGTuS6SfXPf3a7pso/preview", target="_blank") +" or " + A("these patch notes", href="https://www.blackdesertonline.com/news/view/3216", target="_blank"))
 	ret += P("Barter items that you can trade for ship parts unlock as you finish more trades.  You can always trade t1 barter items for verdant stone coupon though.")
 	ret += P("Ship parts used for upgrade need to be full durability.")
+	ret += P("All sea monsters can drop parts for upgrading ships.  There is no list of which drops are where yet. (nov-5")
 	ret += H2("Upgrade Paths") + BR() + CANVAS(id='shipchart', width=820, height=400) + BR()
 	ret += H2("Base Ship Stats")
 	for table in order:
