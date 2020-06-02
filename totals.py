d5a450abd3921c7621f8e0d9151ffe4e83b36a2d
xanthics <3137623+xanthics@users.noreply.github.com>
2020-06-02 11:43:00 -0700

    Add % coin progress (patreon suggestion). changed patreon to image+link and removed script

---
+++ totals.py
@@ -0,0 +1,15 @@
+# Simple dictionary of total coin costs for ships to show a progress %
+
+tccost = {
+	"Information": 0,
+	"Epheria Sailboat": 0,
+	"Improved Epheria Sailboat": 0,
+	"Epheria Caravel": 4160,
+	"Carrack (Advance)": 1164950,
+	"Carrack (Balance)": 1180150,
+	"Epheria Frigate": 0,
+	"Improved Epheria Frigate": 0,
+	"Epheria Galleass": 4000,
+	"Carrack (Volante)": 1344950,
+	"Carrack (Valor)": 1352950
+}
