5483befe4f8a1018e141958df6275d7ee0e47c0c
xanthics <3137623+xanthics@users.noreply.github.com>
2024-06-26 06:46:46 -0700

    changed to reflect new totals

--- totals.py
+++ totals.py
@@ -4,12 +4,12 @@ tccost = {
 	"Information": 0,
 	"Epheria Sailboat": 0,
 	"Improved Epheria Sailboat": 0,
-	"Epheria Caravel": 1160,
+	"Epheria Caravel": 1560,
 	"Carrack (Advance)": 264050,
 	"Carrack (Balance)": 263050,
 	"Epheria Frigate": 0,
 	"Improved Epheria Frigate": 0,
-	"Epheria Galleass": 800,
+	"Epheria Galleass": 1200,
 	"Carrack (Volante)": 291050,
 	"Carrack (Valor)": 292150
 }
